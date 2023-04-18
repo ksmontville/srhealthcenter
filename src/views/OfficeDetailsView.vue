08d69cf912e6205f747e02e265e88a08ff15a9a0
Kyle Montville <ksmontville@gmail.com>
2023-04-18 15:14:29 -0400

    Build out remaining pages and styling changes.

--- src/views/OfficeDetailsView.vue
+++ src/views/OfficeDetailsView.vue
@@ -35,7 +35,7 @@
              max-width="800" height="400" color="accent">
 
       <p class="text-body-1 text-white text-center font-weight-bold my-4">
-        Photos of our South Royalton Health Center
+        Photos of South Royalton Health Center
       </p>
       <small class="my-2">Click to Enlarge</small>
       <v-carousel color="white" cycle continuous>
